332bb60526032a82ff143c7cabf87d2221d2707b
Logon PF Fiap <logonpflocal@fiap.com.br>
2021-12-14 19:56:41 -0300

    conclusao component filter

--- containers/Home.tsx
+++ containers/Home.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useState } from "react";
 import { Filter } from "../components/Filter";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
@@ -8,6 +9,11 @@ type HomeProps = {
 }
 
 export const Home : NextPage<HomeProps> = ({setToken}) => {
+
+    const [previsionDateStart, setPrevisionDateStart] = useState('');
+    const [previsionDateEnd, setPrevisionDateEnd] = useState('');
+    const [status, setStatus] = useState('');
+    const [tasks, setTasks] = useState([]);
 
     const sair = () =>{
         localStorage.removeItem('accessToken');
@@ -19,7 +25,14 @@ export const Home : NextPage<HomeProps> = ({setToken}) => {
     return (
     <>
         <Header sair={sair}/>
-        <Filter />
+        <Filter 
+            previsionDateStart={previsionDateStart}
+            previsionDateEnd={previsionDateEnd}
+            status={status}
+            setPrevisionDateStart={setPrevisionDateStart}
+            setPrevisionDateEnd={setPrevisionDateEnd}
+            setStatus={setStatus}
+        />
         <Footer />
     </>);
 }
