758f52a37fe1ecd090d473957541e854c93f0ca5
Logon PF Fiap <logonpflocal@fiap.com.br>
2021-12-14 22:45:24 -0300

    conclusão dos modais de adição e alteração

--- components/Item.tsx
+++ components/Item.tsx
@@ -4,9 +4,10 @@ import { Task } from "../types/Task";
 
 type ItemProps = {
     task : Task
+    selectTask(task : Task) : void
 }
 
-export const Item : NextPage<ItemProps> = ({ task }) => {
+export const Item : NextPage<ItemProps> = ({ task, selectTask }) => {
 
     const getDateText = (task : Task) => {
         if(task && task.finishDate){
@@ -17,7 +18,8 @@ export const Item : NextPage<ItemProps> = ({ task }) => {
     }
 
     return (
-        <div className={"container-item" + (task?.finishDate ? "" : " active")}>
+        <div className={"container-item" + (task?.finishDate ? "" : " active")}
+            onClick={e => task?.finishDate ? null : selectTask(task)}>
             <img src={task?.finishDate ? '/checked.svg' : 'not-checked.svg'}
                 alt={task?.finishDate ? 'Tarefa Concluída' : 'Tarefa não concluída'}/>
             <div>
